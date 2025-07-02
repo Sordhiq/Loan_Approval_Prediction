21b3658da37dad33fa55e3b2c9c3a3fee867e5bd
Sordhiq <39282470+Sordhiq@users.noreply.github.com>
2025-07-02 22:28:04 +0100

    Update loan_approval.py

--- loan_approval.py
+++ loan_approval.py
@@ -14,7 +14,7 @@ st.set_page_config(
     page_title="Loan Prediction App",
     page_icon="💰",
     layout="centered"
-)
+) 
 
 # -----------------------------------------
 # Gemini API Configuration
